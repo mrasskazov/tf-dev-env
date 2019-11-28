cc416006e07fe61a6db7b707ed45cb4af0ab55b8
Maxim Rasskazov <mrasskazov@mirantis.com>
2019-11-28 20:09:00 +0400

    Attach kernel headers and modules from host as volumes
    Change-Id: Icfa3fe6c9d73364732e57d68b8bdafd33bf32a6b
    Related-PROD: PRODX-1508

--- startup.sh
+++ startup.sh
@@ -241,6 +241,18 @@ if [[ "$own_vm" == '0' ]]; then
     volumes="-v /var/run/docker.sock:/var/run/docker.sock"
     volumes+=" -v ${scriptdir}:/root/tf-dev-env"
     volumes+=" -v ${scriptdir}/container/entrypoint.sh:/root/entrypoint.sh"
+
+    MODULES=($(ls /lib/modules))
+    for MOD in ${MODULES[@]}; do
+      DIR="/usr/src/linux-headers-${MOD}"
+      if [ -d "${DIR}" ]; then
+        volumes+=" -v ${DIR}:${DIR}"
+        volumes+=" -v ${DIR%-*}:${DIR%-*}"
+        M="/lib/modules/${MOD}"
+        volumes+=" -v ${M}:${M}"
+      fi
+    done
+
     if [[ -d "${scriptdir}/config" ]]; then
       volumes+=" -v ${scriptdir}/config:/config"
     fi
