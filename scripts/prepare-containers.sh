98e322158156c042f373b0d8449e73d7f4f14126
Maxim Rasskazov <mrasskazov@mirantis.com>
2019-12-03 14:11:03 +0400

    Implement checkout for contrail-container-builder and contrail-deployers-containers
    Change-Id: I21e1f3898f66aa6e354235b6572a473b564d7c38
    Related-PROD: PRODX-1790

--- scripts/prepare-containers.sh
+++ scripts/prepare-containers.sh
@@ -3,7 +3,17 @@
 REPODIR=/root/src/${CANONICAL_HOSTNAME}/tungsten/contrail-container-builder
 BRANCH=${SB_BRANCH:-mcp/R5.1}
 
+[ -f buildpipeline.env ] && . buildpipeline.env
+
 [ -d ${REPODIR} ] || git clone https://gerrit.mcp.mirantis.com/tungsten/contrail-container-builder -b ${BRANCH}  ${REPODIR}
+
+if [ "${GERRIT_PROJECT##*/}" = "contrail-container-builder" ]; then
+    pushd ${REPODIR} > /dev/null
+        git fetch $(git remote -v | awk '/^origin.*fetch/ {print $2}') ${GERRIT_REFSPEC:?GERRIT_REFSPEC is empty} > /dev/null \
+            && git checkout FETCH_HEAD > /dev/null
+    popd > /dev/null
+fi
+
 for file in tpc.repo.template common.env ; do
   if [ -f $file ]; then
     cp $file ${REPODIR}
--- scripts/prepare-deployers.sh
+++ scripts/prepare-deployers.sh
@@ -1,9 +1,19 @@
 #!/bin/bash -e
 
 REPODIR=/root/src/${CANONICAL_HOSTNAME}/tungsten/contrail-deployers-containers
-BRANCH=${SB_BRANCH:-master}
+BRANCH=${SB_BRANCH:-mcp/R5.1}
 
-[ -d ${REPODIR} ] || git clone https://github.com/Juniper/contrail-deployers-containers -b ${BRANCH}  ${REPODIR}
+[ -f buildpipeline.env ] && . buildpipeline.env
+
+[ -d ${REPODIR} ] || git clone https://gerrit.mcp.mirantis.com/tungsten/contrail-deployers-containers -b ${BRANCH}  ${REPODIR}
+
+if [ "${GERRIT_PROJECT##*/}" = "contrail-deployers-containers" ]; then
+    pushd ${REPODIR} > /dev/null
+        git fetch $(git remote -v | awk '/^origin.*fetch/ {print $2}') ${GERRIT_REFSPEC:?GERRIT_REFSPEC is empty} > /dev/null \
+            && git checkout FETCH_HEAD > /dev/null
+    popd > /dev/null
+fi
+
 for file in tpc.repo.template common.env ; do
   if [ -f $file ]; then
     cp $file ${REPODIR}
